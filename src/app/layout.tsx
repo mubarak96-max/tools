2f66e41d39c0db139511196cbb646687f4b674bc
unknown <mubarakmmm5@gmail.com>
2026-04-09 03:31:27 +0400

    Add Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,24 +50,19 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
 
-        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ? (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `}
-            </Script>
-          </>
-        ) : null}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-056PHFBMP3"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-056PHFBMP3');
+          `}
+        </Script>
       </body>
     </html>
   );
